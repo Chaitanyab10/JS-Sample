5cea1f83be6f3612b94ef5310955a4573d265bae
Chaitanyab10 <bchaitanyadeepika@gmail.com>
2017-04-12 15:29:08 -0500

    Delete user.story.service.js

--- userstory/services/user.story.service.js
+++
@@ -1,8 +0,0 @@
-UserStroyApp.service("userStoryService", userStoryService);
-userStoryService.$inject = ["$http"];
-
-function userStoryService($http) {
-    this.getPosts = function () {
-        return $http.get("http://jsonplaceholder.typicode.com/posts");
-    }
-}
