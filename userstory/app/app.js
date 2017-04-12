55f07e624efc5af4d775e61e26835ea99fb343c9
Chaitanyab10 <bchaitanyadeepika@gmail.com>
2017-04-12 15:28:45 -0500

    Delete app.js

--- userstory/app/app.js
+++
@@ -1,6 +0,0 @@
-
-var UserStroyApp = angular.module("UserStroyApp",["datatables"]);
-$(document).ready(function(){
-	$('div.dataTables_filter input').addClass('form-control');
-$('div.dataTables_length select').addClass('form-control');
-});
